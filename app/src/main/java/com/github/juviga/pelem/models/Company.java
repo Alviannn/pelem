b48a544a85bfc125868d0d738b43efdfb90b87f2
Alviannn <alvianndq@gmail.com>
2022-06-17 16:16:17 +0700

    Add movie models

---
+++ app/src/main/java/com/github/juviga/pelem/models/Company.java
@@ -0,0 +1,30 @@
+package com.github.juviga.pelem.models;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Company {
+
+    private int id;
+    private String name;
+
+    @SerializedName("origin_country")
+    private String country;
+
+    @Nullable
+    @SerializedName("logo_path")
+    private String logoPath;
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "Company{" +
+               "id=" + id +
+               ", name='" + name + '\'' +
+               ", country='" + country + '\'' +
+               ", logoPath='" + logoPath + '\'' +
+               '}';
+    }
+}
